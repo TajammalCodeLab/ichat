dc10c7272b1b6fce64dd5295e18d49e4fa7db14f
Tajammal <managebydev@gmail.com>
2025-04-15 22:37:03 +0500

    App iniatil commits added structure

---
+++ ichat/Mian/ichatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ichatApp.swift
+//  ichat
+//
+//  Created by SID on 15/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ichatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
